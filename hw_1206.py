e6b059c7021064177ac10414813a0f7d17dfb2d3
lawisorder <szhukov1001@gmail.com>
2022-06-14 21:42:58 +0300

    add hw 13.6 v 1.0

--- hw_1206.py
+++ hw_1206.py
@@ -1,17 +1,26 @@
-import random
-# 13.4
-
-def f(step, start, stop):
-    sp = []
-    while step != 0:
-        a = random.randint(start, stop)
-        step -= 1
-        sp.append(a)
-    return sp
-
-
-step, start, stop = int(input()), int(input()), int(input())
-print(f(step, start, stop))
+# import random
+# # 13.4
+#
+# def f(step, start, stop):
+#     sp = []
+#     while step != 0:
+#         a = random.randint(start, stop)
+#         step -= 1
+#         sp.append(a)
+#     return sp
+#
+#
+# step, start, stop = int(input()), int(input()), int(input())
+# print(f(step, start, stop))
 
 # 13.5
+# вариант без рекурсии
+def find_factorial(num):
+    a = 1
+    for i in range(1, num+1):
+        a *= i
+    return a
 
+print(find_factorial(int(input())))
+
+# вариант с рекурсией
